9d9709467e48ec0e31cde3b9e5609c5680544814
Shane Milton <Shane.Milton@CleanSlateTG.com>
2023-02-27 12:01:37 -0500

    Potentially complete application.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestHelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public string CustomGreeting => Configuration["CustomGreeting"];
+
+
+        public HelloController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // GET: api/<HelloController>
+        [HttpGet]
+        public string Get()
+        {
+            return $"{CustomGreeting} Mystery User!";
+        }
+
+        // GET api/<HelloController>/5
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return $"{CustomGreeting} {name}!";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rest-helloworld
+# RestHelloWorld
+
+This is a .NET 6 MVC API application that implements nothing more than Hello World GET requests. Swagger docs & Swagger UI is a part of this.
+
+This application is meant to be reused for quick proof-of-concept scenarios and is intended to be configurable in some unique ways.
+
+Specifically, I plan to hook this Github repo to Azure Deployments to feed proof-of-concept applications hosted in Azure App Services.
